5750426a2046a6817686ffb3797565006919a9f9
Anton Veldanov <anton.veldanov@gmail.com>
2022-08-11 19:40:44 -0700

    iOS

--- MessageMeApp/Controllers/Login/LoginViewController.swift
+++ MessageMeApp/Controllers/Login/LoginViewController.swift
@@ -9,23 +9,45 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "messageLogo")
+        imageView.contentMode = .scaleAspectFit
+
+        return imageView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Log In"
 
         view.backgroundColor = .red
 
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+
+        // MARK: - Add Subviews
+        view.addSubview(imageView)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.frame.size.width/3
+        imageView.frame = CGRect(x: (view.frame.size.width - size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
     }
     
 
-    /*
-    // MARK: - Navigation
+    @objc private func didTapRegister(){
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true)
     }
-    */
 
 }
---
+++ MessageMeApp/Resources/Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  Extensions.swift
+//  MessageMeApp
+//
+//  Created by Anton Veldanov on 8/11/22.
+//
+
+import UIKit
+
+
+extension UIView {
+
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+
+
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+}
