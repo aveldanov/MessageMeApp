0ddc180fecb852a06e96daad73182024fc3ff8f6
Anton Veldanov <anton.veldanov@gmail.com>
2022-08-11 19:55:35 -0700

    iOS

--- MessageMeApp/Controllers/Login/LoginViewController.swift
+++ MessageMeApp/Controllers/Login/LoginViewController.swift
@@ -18,28 +18,56 @@ class LoginViewController: UIViewController {
         return imageView
     }()
 
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email address..."
+        return field
+    }()
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Log In"
-
         view.backgroundColor = .red
-
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
 
         // MARK: - Add Subviews
+        view.addSubview(scrollView)
         view.addSubview(imageView)
+        view.addSubview(emailField)
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let size = view.frame.size.width/3
-        imageView.frame = CGRect(x: (view.frame.size.width - size)/2,
+        scrollView.frame = view.bounds
+
+
+        let size = scrollView.width/3
+        imageView.frame = CGRect(x: (scrollView.width-size)/2,
                                  y: 20,
                                  width: size,
                                  height: size)
+
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom+10,
+                                  width: scrollView.width-60,
+                                 height: 52)
     }
     
 
