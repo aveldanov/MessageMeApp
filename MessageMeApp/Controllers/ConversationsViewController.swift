17f49feaf1062045abc608f63fd8e8f21beb4bef
Anton Veldanov <aveldanov@linkedin.com>
2022-08-11 19:19:33 -0700

    iOS

--- MessageMeApp/Controllers/ConversationsViewController.swift
+++ MessageMeApp/Controllers/ConversationsViewController.swift
@@ -14,6 +14,20 @@ class ConversationsViewController: UIViewController {
         view.backgroundColor = .blue
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true, completion: nil)
+        }
+
+    }
+
 
 }
 
